44f17f9658dfda722e9d665ce0f2094345492707
diogomotta <mottad216@gmail.com>
2026-03-13 13:12:42 +0100

    readme updated

--- color_point.py
+++ color_point.py
@@ -8,6 +8,12 @@ class ColorPoint(Point):
         self.x = x
         self.y = y
         self.color = color
+    def __str__(self):
+        """
+        Overriding __str__ method.
+        :return: string representation of ColorPoint.
+        """
+        return f"{self.color}: P<{self.x},{self.y},{self.color}>"
 
 p1 = ColorPoint(1, 2, 'red')
 print(p1.color)
