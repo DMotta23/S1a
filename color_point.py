0eab61078b0b46024347ebb6652899a5582dcd84
diogomotta <mottad216@gmail.com>
2026-03-20 10:54:48 +0100

    adding isinstance()

--- color_point.py
+++ color_point.py
@@ -5,6 +5,11 @@ class ColorPoint(Point):
     Color point class inherits from Point class.
     """
     def __init__(self, x, y, color):
+        # check if x and y are numbers
+        if not isinstance(x, (int, float)):
+            raise TypeError("x must be an number")
+        if not isinstance(y, (int, float)):
+            raise TypeError("y must be an number")
         self.x = x
         self.y = y
         self.color = color
@@ -15,6 +20,15 @@ class ColorPoint(Point):
         """
         return f"{self.color}: P<{self.x},{self.y},{self.color}>"
 
-p1 = ColorPoint(1, 2, 'red')
+p1 = ColorPoint(1, 2, "red")
+p2 = ColorPoint(3, 4, "green")
+p3 = ColorPoint(5, 6, "blue")
+p4 = ColorPoint(-2, -3, "yellow")
+color_points = [p1, p2, p3, p4]
 print(p1.color)
 print(p1)
+print(color_points)
+color_points.sort()
+print(color_points)
+# p5 = ColorPoint("bob", "james", "red")
+# print(p5)
